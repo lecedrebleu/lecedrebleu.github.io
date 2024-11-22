c86063b3e508debcc79f4ee770fa5571140fd658
Witold <dev@witold.fr>
2024-11-22 13:33:13 +0100

    disable matomo in dev mode

--- src/components/matomo.tsx
+++ src/components/matomo.tsx
@@ -2,15 +2,17 @@ import React from "react";
 
 export default function Matomo() {
   React.useEffect(() => {
-    var _mtm = ((window as any)._mtm = (window as any)._mtm || []);
-    _mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" });
-    var d = document,
-      g = d.createElement("script"),
-      s = d.getElementsByTagName("script")[0];
-    g.async = true;
-    g.src =
-      "https://cdn.matomo.cloud/lecedrebleupx.matomo.cloud/container_680TaRAp.js";
-    s.parentNode?.insertBefore(g, s);
+    if (process.env.NODE_ENV !== "development") {
+      var _mtm = ((window as any)._mtm = (window as any)._mtm || []);
+      _mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" });
+      var d = document,
+        g = d.createElement("script"),
+        s = d.getElementsByTagName("script")[0];
+      g.async = true;
+      g.src =
+        "https://cdn.matomo.cloud/lecedrebleupx.matomo.cloud/container_680TaRAp.js";
+      s.parentNode?.insertBefore(g, s);
+    }
   }, []);
   return <></>;
 }
