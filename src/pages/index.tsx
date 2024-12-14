299be7f2ad6a1852589c0e6ef5da6699905bb721
Witold <dev@witold.fr>
2024-12-14 14:40:38 +0100

    feat: past and future events

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,47 @@
 import * as React from "react";
-import { Link, graphql } from "gatsby";
+import { Link, PageProps, graphql } from "gatsby";
 
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 import Newsletter from "../components/newsletter";
 
-const BlogIndex = ({ data, location }) => {
+interface Post {
+  excerpt: string;
+  frontmatter: {
+    title: string;
+    date: string;
+    tags: string[];
+    shortDescription?: string;
+    description?: string;
+    image?: string;
+    reservationEnabled?: boolean;
+  };
+  fields: {
+    slug: string;
+  };
+}
+
+interface BlogIndexProps {
+  allMarkdownRemark: {
+    nodes: Post[];
+  };
+  site: {
+    siteMetadata?: {
+      title?: string;
+    };
+  };
+}
+
+const formatter = new Intl.DateTimeFormat("fr-FR", {
+  weekday: "short",
+  year: "numeric",
+  month: "long",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+});
+
+const BlogIndex = ({ data, location }: PageProps<BlogIndexProps>) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`;
   const posts = data.allMarkdownRemark.nodes;
 
@@ -17,52 +53,115 @@ const BlogIndex = ({ data, location }) => {
     );
   }
 
+  const beforeNowIndex = posts.findIndex(
+    (p) => new Date(p.frontmatter.date) < new Date()
+  );
+
+  let pastEvents: Post[], futureEvents: Post[];
+  if (beforeNowIndex === 0 || beforeNowIndex === -1) {
+    pastEvents = posts;
+    futureEvents = [];
+  } else {
+    [futureEvents, pastEvents] = [
+      posts.slice(0, beforeNowIndex),
+      posts.slice(beforeNowIndex),
+    ];
+  }
+
   return (
     <Layout location={location} title={siteTitle}>
       <Newsletter />
-      <ol style={{ listStyle: `none` }}>
-        {posts.map((post) => {
-          const title = post.frontmatter.title || post.fields.slug;
-
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <p>
-                    <small>{post.frontmatter.date}</small>
-                  </p>
-                  <p>
-                    <small>{post.frontmatter.tags.join(" / ")}</small>
-                  </p>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html:
-                        post.frontmatter.shortDescription ||
-                        post.frontmatter.description ||
-                        post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
-          );
-        })}
-      </ol>
+      {futureEvents && futureEvents.length > 0 && (
+        <div>
+          <h3>Prochains évènements</h3>
+          <ol style={{ listStyle: `none` }}>
+            {futureEvents.map((post) => (
+              <li key={post.fields.slug}>
+                <EventItem
+                  title={post.frontmatter.title || post.fields.slug}
+                  slug={post.fields.slug}
+                  tags={post.frontmatter.tags}
+                  date={new Date(post.frontmatter.date)}
+                  description={
+                    post.frontmatter.shortDescription ||
+                    post.frontmatter.description ||
+                    post.excerpt
+                  }
+                />
+              </li>
+            ))}
+          </ol>
+        </div>
+      )}
+      {pastEvents && pastEvents.length > 0 && (
+        <div>
+          <h3>Évènements passés</h3>
+          <ol style={{ listStyle: `none` }}>
+            {pastEvents.map((post) => (
+              <li key={post.fields.slug}>
+                <EventItem
+                  title={post.frontmatter.title || post.fields.slug}
+                  slug={post.fields.slug}
+                  tags={post.frontmatter.tags}
+                  date={new Date(post.frontmatter.date)}
+                  description={
+                    post.frontmatter.shortDescription ||
+                    post.frontmatter.description ||
+                    post.excerpt
+                  }
+                />
+              </li>
+            ))}
+          </ol>
+        </div>
+      )}
     </Layout>
   );
 };
+
+function EventItem({
+  title,
+  slug,
+  tags,
+  date,
+  description,
+}: {
+  title: string;
+  slug: string;
+  tags: string[];
+  date: Date;
+  description: string;
+}) {
+  return (
+    <article
+      className="post-list-item"
+      itemScope
+      itemType="http://schema.org/Article"
+    >
+      <header>
+        <h2>
+          <Link to={slug} itemProp="url">
+            <span itemProp="headline">{title}</span>
+          </Link>
+        </h2>
+        <p>
+          <small>{formatter.format(date)}</small>
+        </p>
+        <p>
+          <small>{tags.join(" / ")}</small>
+        </p>
+      </header>
+      <section>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: description,
+          }}
+          itemProp="description"
+        />
+      </section>
+    </article>
+  );
+}
 
 export default BlogIndex;
 
@@ -80,17 +179,14 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(
-      sort: { frontmatter: { date: ASC } }
-      filter: { isFuture: { eq: true } }
-    ) {
+    allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
       nodes {
         excerpt
         fields {
           slug
         }
         frontmatter {
-          date(formatString: "DD MMMM YYYY, H:mm", locale: "fr")
+          date
           title
           description
           tags
