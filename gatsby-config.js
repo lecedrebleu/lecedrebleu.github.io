79b4994666d24c6faa7004a6c9012e0758821f1f
Witold <dev@witold.fr>
2025-01-25 19:15:42 +0100

    change pine tree logo to real logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -115,7 +115,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/pine-tree.svg`, // This path is relative to the root of the site.
+        icon: `src/images/logo-small.png`, // This path is relative to the root of the site.
       },
     },
   ],
