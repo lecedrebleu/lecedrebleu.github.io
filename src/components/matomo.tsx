99038e60a7efd377fe6dbf1c0f02b4ba19b52b0d
Witold <dev@witold.fr>
2025-01-29 10:56:56 +0100

    feat: update Matomo tracking implementation

--- src/components/matomo.tsx
+++ src/components/matomo.tsx
@@ -3,15 +3,20 @@ import React from "react";
 export default function Matomo() {
   React.useEffect(() => {
     if (process.env.NODE_ENV !== "development") {
-      var _mtm = ((window as any)._mtm = (window as any)._mtm || []);
-      _mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" });
-      var d = document,
-        g = d.createElement("script"),
-        s = d.getElementsByTagName("script")[0];
-      g.async = true;
-      g.src =
-        "https://cdn.matomo.cloud/lecedrebleupx.matomo.cloud/container_680TaRAp.js";
-      s.parentNode?.insertBefore(g, s);
+      var _paq = ((window as any)._mtm = (window as any)._mtm || []);
+      _paq.push(["trackPageView"]);
+      _paq.push(["enableLinkTracking"]);
+      (function () {
+        var u = "https://matomo.lecedrebleu-px.fr/";
+        _paq.push(["setTrackerUrl", u + "matomo.php"]);
+        _paq.push(["setSiteId", "1"]);
+        var d = document,
+          g = d.createElement("script"),
+          s = d.getElementsByTagName("script")[0];
+        g.async = true;
+        g.src = u + "matomo.js";
+        s.parentNode?.insertBefore(g, s);
+      })();
     }
   }, []);
   return <></>;
